5be1bb538b20523ec2c0cabfa308d549b94cf4b3
waranyadatong <57949212+waranyadatong@users.noreply.github.com>
2022-11-24 18:52:03 +0700

    Added Batch File
    Coding Batch for SMT MOTB-A7

---
+++ BatchFile-MOTB-A7/SUSZ-018MW-0B.bat
@@ -0,0 +1,22 @@
+@if (@CodeSection == @Batch) @then
+
+@echo off
+
+set "$process=putty.exe"
+for %%a in (%$process%) do taskkill /f /im %%a 2>nul
+
+rem Use %SendKeys% to send keys to the keyboard buffer
+set SendKeys=CScript //nologo //E:JScript "%~F0"
+
+rem Start the putty window with the user name only
+start C:\PuTTY\putty.exe -ssh fujikura@192.168.75.44 -pw 1234
+
+rem Send the password to putty window
+%SendKeys% "cd /home/fujikura/Desktop/Product{ENTER}"
+
+%SendKeys% "python SUSZ-018MW-0B.py{ENTER}"
+goto :EOF
+
+@end
+
+WScript.CreateObject("WScript.Shell").SendKeys(WScript.Arguments(0));
